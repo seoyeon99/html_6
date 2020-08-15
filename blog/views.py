cda138d105c5b339911651d4186b96b7fa002606
Yeon Ji Seo <duswl7840@naver.com>
2020-08-15 23:41:08 +0900

    ListView 활용하기

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
-from. models import Post
+from .models import Post
+from django.views.generic import ListView
+
+class PostList(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.order_by('-created')
+
 
 # Create your views here.
 
-def index(request):
-    posts = Post.objects.all()
-    return render(
-        request,
-        'blog/index.html',
-        {
-          'posts':posts,
-        }
-
-    )
+# def index(request):
+#     posts = Post.objects.all()
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#           'posts':posts,
+#         }
+#
+#     )
