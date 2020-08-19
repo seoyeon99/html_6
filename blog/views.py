05d1d364b8401d41807daa12a02346377b47cb01
AYoung Eom <aatma413@naver.com>
2020-08-20 01:08:26 +0900

    'category'

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView
 
+
 class PostList(ListView):
     model = Post
 
     def get_queryset(self):
         return Post.objects.order_by('-created')
+
+def category_woman(request):
+    return render(request, 'category_woman.html')
 
 
 # Create your views here.
