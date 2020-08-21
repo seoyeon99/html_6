bb242ef02d0376e084fa91a5cb290af71ecf27bb
duswl7840 <68327163+duswl7840@users.noreply.github.com>
2020-08-22 00:36:19 +0900

    Update urls.py

--- my_site_prj/urls.py
+++ my_site_prj/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name="login/index.html")),
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls'))
